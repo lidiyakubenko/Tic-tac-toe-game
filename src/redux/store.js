9cc9a993447a80a7b6dda497fefe16a8469e2f4c
lidkapro <44070274+lidkapro@users.noreply.github.com>
2019-06-19 16:22:48 +0300

    computer make goal reducer and action

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,24 @@ import {createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import game from './reducers'
 import {createStore, applyMiddleware} from 'redux'
+import c from '../../constants'
+import {determineWinner} from './actions'
+
+
+
+const actionsAfterMyGoal = store => next => action => {
+    let result
+    result = next(action)
+    if (action.type === c.MAKE_GOAL) {
+        const gameField = store.getState().gameField
+        store.dispatch(determineWinner(gameField))
+    }
+    return result
+}
 
 
 let configureStore = () => createStore(
-    game, {}, applyMiddleware(thunk)
+    game, {}, applyMiddleware(thunk, actionsAfterMyGoal)
 )
 
 export default configureStore
