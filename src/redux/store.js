b4446349c42c7dd0b821d66175d7c494ecfba955
lidkapro <44070274+lidkapro@users.noreply.github.com>
2019-06-19 14:50:28 +0300

    Make goal reducer and action

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
 import {createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import game from './reducers'
+import {createStore, applyMiddleware} from 'redux'
+
 
 let configureStore = () => createStore(
     game, {}, applyMiddleware(thunk)
